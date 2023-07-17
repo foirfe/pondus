bfb408f474ad33273016d32d2ba69ae6aa4180ae
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2023-07-17 18:02:37 +0200

    implementation of helmet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,11 @@
+import { Helmet } from "react-helmet";
+
 export default function Home() {
-  return <div className="index-page"></div>;
+  return (
+    <div className="index-page">
+      <Helmet>
+        <title>Pondus - Puds og Polering</title>
+      </Helmet>
+    </div>
+  );
 }
